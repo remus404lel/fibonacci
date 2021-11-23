47ee80c7ef647d5c3721f687918f2b61305ca209
Remus Barbulescu <remus@yarooms.com>
2021-11-23 11:17:06 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const keys = require('./keys');
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Express app setup
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// Postgres client setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on("connect", (client) => {
+    client
+      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+      .catch((err) => console.error(err));
+});
